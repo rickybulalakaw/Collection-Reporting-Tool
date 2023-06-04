2e54284d593406695687a59a3fc0c72d4446df45
Ricky Bulalakaw <rbulalakaw@gmail.com>
2023-06-05 07:15:45 +0800

    Updated user name in welcome

--- resources/views/components/dashboard/welcome-banner.blade.php
+++ resources/views/components/dashboard/welcome-banner.blade.php
@@ -44,7 +44,7 @@
 
     <!-- Content -->
     <div class="relative">
-        <h1 class="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">Good afternoon, Ricky 👋</h1>
+        <h1 class="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">Good afternoon, {{ Auth::user()->name }} 👋</h1>
         <p class="dark:text-indigo-200">Here is what's happening with your projects today:</p>
     </div>
 
