6ac061c914f0a601f6e66a6c120d747a85141027
Ricky Bulalakaw <rbulalakaw@gmail.com>
2023-06-08 22:13:14 +0800

    tailwind fixes

--- resources/views/accountableForm/create.blade.php
+++ resources/views/accountableForm/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-<div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<div class="mx-auto w-8/12 sm:pt-0 bg-gray-100 dark:bg-gray-900">
 
- <h1 class="text-center text-lg text-blue-500 font-bold mb-5 ">Register Accountable Forms</h1>
+ <h1 class="text-center text-lg text-black font-bold mb-5 ">Register Accountable Forms</h1>
  <form action="{{ route('create-accountable-form') }}" method="post">
   @csrf
 
--- resources/views/accountableFormType/create.blade.php
+++ resources/views/accountableFormType/create.blade.php
@@ -2,7 +2,7 @@
 
 <div class=" mx-auto w-8/12 dark:bg-gray-900">
 
-<h2 class="text-center text-lg text-blue-500 font-bold mb-5">Create Accountable Form Type</h2>
+<h2 class="text-center text-lg text-black font-bold mb-5">Create Accountable Form Type</h2>
 
  <form action="{{ route('create-accountable-form-type')}}" method="post">
  @csrf
@@ -15,19 +15,19 @@
 </div>
 
  <div class="col-span-6 sm:col-span-4 mb-4">
-    <x-label for="number" value="{{ __('Number (as designated by BIR)') }}" />
+    <x-label for="number" value="{{ __('Form Number') }}" />
     <x-input id="number" name="number" type="text" class="mt-1 block w-full" wire:model.defer="state.number" autocomplete="number" />
     <x-input-error for="number" class="mt-2" />
 </div>
 
  <div class="col-span-6 sm:col-span-4 mb-4">
-    <x-label for="default_amount" value="{{ __('Default Amount') }}" />
+    <x-label for="default_amount" value="{{ __('Default Amount (Optional)') }}" />
     <x-input id="default_amount" name="default_amount" type="number" class="mt-1 block w-full" wire:model.defer="state.default_amount" autocomplete="default_amount" />
     <x-input-error for="default_amount" class="mt-2" />
 </div>
 
 <x-button class="bg-sky-600 hover:bg-sky-700   " type="submit" wire:click="submit">
-    {{ __('Save') }}
+    {{ __('Create') }}
 </x-button>
 
  </form>
