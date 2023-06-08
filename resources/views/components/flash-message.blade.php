cfa593084961ecfa511a830d3d4ca23b5692fd20
Ricky Bulalakaw <rbulalakaw@gmail.com>
2023-06-08 22:12:51 +0800

    updated all flash to use tailwind

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,6 +1,5 @@
 <div x-data="{show:true}" x-init="setTimeout(()=>show=false,3000)" x-show="show" class=""> 
 @if (session()->has('success'))
-<!-- <p class="alert alert-success text-center"> -->
 <div
   class="mb-4 rounded-lg bg-blue-600 px-6 py-5 text-base text-blue-100 w-1/2 mx-auto "
   role="alert">
@@ -8,13 +7,21 @@
 </div>
 @elseif (session()->has('error'))
 <div
-  class="mb-4 rounded-lg bg-danger-100 px-6 py-5 text-base text-danger-700"
+  class="mb-4 rounded-lg bg-red-600 px-6 py-5 text-base text-white"
   role="alert">
   {{ session()->get('error') }}
 </div>
 @elseif (session()->has('warning'))
-<p {{ $attributes->merge(['class' => 'text-sm text-yellow-600 dark:text-red-400']) }}>{{ session()->get('warning') }}</p>
+<div
+  class="mb-4 rounded-lg bg-yellow-500  px-6 py-5 text-base text-white"
+  role="alert">
+  {{ session()->get('warning') }}
+</div>
 @elseif (session()->has('message'))
-<p {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400']) }}>{{ session()->get('message') }}</p>
+<div
+  class="mb-4 rounded-lg bg-blue-600  px-6 py-5 text-base text-white"
+  role="alert">
+  {{ session()->get('message') }}
+</div>
 @endif 
 </div>
