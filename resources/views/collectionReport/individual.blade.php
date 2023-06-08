2c14d46a33997594a5572dacacba233ef6f54685
Ricky Bulalakaw <rbulalakaw@gmail.com>
2023-06-09 07:49:25 +0800

    button fix

--- resources/views/collectionReport/individual.blade.php
+++ resources/views/collectionReport/individual.blade.php
@@ -1,3 +1,4 @@
+
 <x-app-layout>
  <div class="mx-auto w-8/12">
        
@@ -7,12 +8,13 @@
  
  <table class="table-auto w-full border-collapse border-gray-200 dark:border-gray-700  mb-5 px-6 py-3  ">
   <thead class=" dark:bg-gray-800  ">
-   <tr class="border-b bg-blue-500 border-gray-200 dark:border-gray-700">
+   <tr class="border-b bg-gray-400 text-white border-gray-200 dark:border-gray-700 dark:bg-gray-900">
     <th class="px-6 py-3">Accountable Form Type</th>
     <th class="px-6 py-3">Accountable Form Number</th>
     <th class="px-6 py-3">Used?</th>
     <th class="px-6 py-3">Payor </th>
     <th class="px-6 py-3">Amount</th>
+    <th class="px-6 py-3">Review Functions</th>
    </tr>
   </thead>
 
@@ -23,6 +25,18 @@
     <td class="px-6 py-3"> Yes</td>
     <td class="px-6 py-3"> George Washington</td>
     <td class="text-right px-6 py-3 "> 1,500</td>
+    @can('collector')
+    <td class="px-6 py-3">
+     <a href="#" class="text-blue-500">
+      <i class="fas fa-edit"></i>
+      Edit
+      </a>
+      <a href="#" class="text-red-500">
+       <i class="fas fa-trash"></i>
+       Delete
+       </a>
+       </td>
+    @endcan
    </tr>
    <tr  class="border-b border-gray-200 dark:border-gray-700">
     <td class="px-6 py-3"> AF 51 </td>
@@ -30,6 +44,18 @@
     <td class="px-6 py-3"> Yes</td>
     <td class="px-6 py-3"> George Del Pilar</td>
     <td class="text-right px-6 py-3"> 1,550</td>
+    @can('collector')
+    <td class="px-6 py-3">
+     <a href="#" class="text-blue-500">
+      <i class="fas fa-edit"></i>
+      Edit
+      </a>
+      <a href="#" class="text-red-500">
+       <i class="fas fa-trash"></i>
+       Delete
+      </a>
+     </td>
+     @endcan
    </tr>
    <tr  class="border-b border-gray-200 dark:border-gray-700">
     <td class="px-6 py-3"> Cash Ticket </td>
@@ -37,6 +63,24 @@
     <td class="px-6 py-3"> Yes</td>
     <td class="px-6 py-3"> Juan Tamad</td>
     <td class="text-right px-6 py-3"> 10</td>
+    @can('collector')
+    <td class="px-6 py-3  text-center">
+     <x-button class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+     <a href="#" class=" ">
+      <i class="fas fa-edit"></i>
+      
+      </a>
+      </x-button>
+
+      <x-button class="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+      <a href="#">
+       <i class="fas fa-trash"></i>
+       
+      </a>
+      </x-button>
+      
+     </td>
+     @endcan
    </tr>
    <tr  class="border-b border-gray-200 dark:border-gray-700">
     <td class="px-6 py-3"> Cash Ticket </td>
@@ -44,10 +88,37 @@
     <td class="px-6 py-3"> No</td>
     <td class="px-6 py-3"> </td>
     <td class="text-right px-6 py-3"> 0</td>
+    @can('collector')
+    <td class="px-6 py-3">
+     <a href="#" class="text-blue-500">
+      <i class="fas fa-edit"></i>
+      
+      </a>
+      <a href="#" class="text-red-500">
+       <i class="fas fa-trash"></i>
+      </a>
+     </td>
+     @endcan
+
+    @can('consolidator')
+    <td class="px-6 py-3 text-center">
+     <x-button class=" bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  ">
+     <a href="#" class="text-blue-500   ">
+      <x-fas class="fas fa-eye text-white" />
+      
+      </a>
+     </x-button>
+     </td>
+     @endcan
    </tr>
-   <tr class="border-b border-gray-200 dark:border-gray-700">
+   <tr class="border-b bg-gray-200 font-bold border-gray-200 dark:border-gray-700">
     <td class="px-6 py-3" colspan="4"> Subtotal </td>
+    @if(auth()->user()->function == 2)
     <td class="text-right px-6 py-3"> 3,060</td>
+    <td class="text-right px-6 py-3"></td>
+    @else
+    <td class="text-right px-6 py-3"> 3,060</td>
+    @endif
    </tr>
   </tbody>
 
